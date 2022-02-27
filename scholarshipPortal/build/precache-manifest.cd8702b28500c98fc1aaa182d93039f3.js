471d2b4a595d268c9783ea2315b07ac68cc934a1
nporia <Porianikita262@gmail.com>
2022-02-27 13:25:13 +0530

    Add files via upload

---
+++ scholarshipPortal/build/precache-manifest.cd8702b28500c98fc1aaa182d93039f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54738528373eec213929c7a513bc459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b1c0aec8028659ae5dc",
+    "url": "/static/css/main.58b50354.chunk.css"
+  },
+  {
+    "revision": "541114c439d914286c97",
+    "url": "/static/js/2.75907e75.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.75907e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1c0aec8028659ae5dc",
+    "url": "/static/js/main.fafa42d1.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  },
+  {
+    "revision": "9528c041d83f09874f12dba539f8f784",
+    "url": "/static/media/img.9528c041.png"
+  },
+  {
+    "revision": "4ecab4e74f43d29a15614585833a9adb",
+    "url": "/static/media/logo.4ecab4e7.png"
+  }
+]);
